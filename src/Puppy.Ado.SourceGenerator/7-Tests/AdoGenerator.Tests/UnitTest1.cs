ca5ba74fd32f3bb8852050130e1f1029481aafc5
The Juanito Learns Show <98781316+TheJuanitoLearnsShow@users.noreply.github.com>
2025-12-06 22:21:43 -0500

    Test WIP

--- src/Puppy.Ado.SourceGenerator/7-Tests/AdoGenerator.Tests/UnitTest1.cs
+++ src/Puppy.Ado.SourceGenerator/7-Tests/AdoGenerator.Tests/UnitTest1.cs
@@ -1,9 +1,21 @@
-﻿namespace AdoGenerator.Tests;
+﻿using Puppy.Ado.SourceGenerator;
+
+namespace AdoGenerator.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    private const string ConnectionName = "AdoGenerator_SampleDb";
+    private static string GetConnectionString()
     {
+        return Environment.GetEnvironmentVariable(ConnectionName) ?? 
+               $"Data Source=.\\sqlExpress;Database=AutomatedTESTS_{ConnectionName};Integrated Security=True;TrustServerCertificate=True;";
+    }
+    [Fact]
+    public async Task Test_Model()
+    {
+        var connStr = GetConnectionString();
+        var reader = new SqlServerSchemaReader(connStr);
+        var model = await reader.ReadAsync();
+        Assert.NotNull(model);
     }
 }
